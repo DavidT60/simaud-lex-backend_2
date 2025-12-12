3aab41c5690b1d1d051a003aa874e04434832805
drivera <davusitoriver03@gmail.com>
2025-12-11 21:41:46 -0400

    Pre Production ENC

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,24 @@ import { PersonModule } from './person/person.module';
 import { NnaModule } from './nna/nna.module';
 import { ProcesoJudicialModule } from './proceso-judicial/proceso-judicial.module';
 import { CommonModule } from './common/common.module';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'my_db_uni',
-      autoLoadEntities: true,
-      synchronize: true, // only for dev
-    }),
+      ConfigModule.forRoot({
+        isGlobal:true
+      }),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (config: ConfigService) => ({
+          type: "postgres",
+          url: config.get<string>("DATABASE_URL"),
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+        inject: [ConfigService],
+      }),
     UserModule,
     AuthModule,
     PersonModule,
