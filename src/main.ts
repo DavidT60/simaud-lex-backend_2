40349562512d09c6acdd99414a47984482c8d1a2
DavidT60 <davusitoriver03@gmail.com>
2025-12-12 01:05:39 -0400

    Allow Oring *

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,38 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './common/exceptions/all-exceptions.filter';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AllExceptionsFilter } from "./common/exceptions/all-exceptions.filter";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS for frontend
+  // allow requests from any origin
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://simaud-lex.netlify.app'],
+    origin: "*",
     credentials: true,
   });
 
   app.useGlobalFilters(new AllExceptionsFilter());
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
 
   const config = new DocumentBuilder()
-    .setTitle('SIMAUD-LEX API')
-    .setDescription('API para el Sistema de Protección de Niños, Niñas y Adolescentes')
-    .setVersion('1.0')
+    .setTitle("SIMAUD-LEX API")
+    .setDescription(
+      "API para el Sistema de Protección de Niños, Niñas y Adolescentes"
+    )
+    .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
